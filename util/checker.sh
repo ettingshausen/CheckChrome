942e2a39db27aff09f6fd89d361e8b6a71c81086
ettingshausen <ettingshausen@outlook.com>
2023-11-07 09:34:07 +0800

    os_version to 10

--- util/checker.sh
+++ util/checker.sh
@@ -4,12 +4,15 @@ echo '-------------------------------------'
 
 echo 'Checking Chrome Stable x86 ......'
 
+
+os_version=10
+
 curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' encoding='UTF-8'?>
             <request protocol='3.0' version='1.3.23.9' shell_version='1.3.21.103' ismachine='0'
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='10' arch='x86'/>
+            <os platform='win' version='${os_version}' arch='x86'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='-multi-chrome' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
@@ -24,7 +27,7 @@ curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' e
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='6.3' arch='x64'/>
+            <os platform='win' version='${os_version}' arch='x64'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='x64-stable-multi-chrome' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
@@ -39,7 +42,7 @@ curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' e
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='6.3' arch='x86'/>
+            <os platform='win' version='${os_version}' arch='x86'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='1.1-beta' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
@@ -54,7 +57,7 @@ curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' e
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='6.3' arch='x64'/>
+            <os platform='win' version='${os_version}' arch='x64'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='x64-beta-multi-chrome' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
@@ -69,7 +72,7 @@ curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' e
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='6.3' arch='x86'/>
+            <os platform='win' version='${os_version}' arch='x86'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='2.0-dev' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
@@ -84,7 +87,7 @@ curl -s "https://tools.google.com/service/update2" --data "<?xml version='1.0' e
                      sessionid='{3597644B-2952-4F92-AE55-D315F45F80A5}' installsource='ondemandcheckforupdate'
                      requestid='{CD7523AD-A40D-49F4-AEEF-8C114B804658}' dedup='cr'>
             <hw sse='1' sse2='1' sse3='1' ssse3='1' sse41='1' sse42='1' avx='1' physmemory='12582912' />
-            <os platform='win' version='6.3' arch='x64'/>
+            <os platform='win' version='${os_version}' arch='x64'/>
             <app appid='{8A69D345-D564-463C-AFF1-A69D9E530F96}' ap='x64-dev-multi-chrome' version='' nextversion='' lang='' brand='GGLS' client=''>
                 <updatecheck/>
             </app>
